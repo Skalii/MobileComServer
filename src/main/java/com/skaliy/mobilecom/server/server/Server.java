06d0da6d40e372cd03ee16f80822760cd325af71
dima <skaliy.d@gmail.com>
2017-11-15 23:57:16 +0200

    create queries

--- src/main/java/com/skaliy/mobilecom/server/server/Server.java
+++ src/main/java/com/skaliy/mobilecom/server/server/Server.java
@@ -47,12 +47,17 @@ public class Server implements Runnable {
     static String[][] getResult(String query) throws SQLException {
         String[][] result = new String[0][];
 
-        String _query = query;
+        String _query = query, parameter = "";
         int index = 0;
 
         if (_query.startsWith("get_tariff_")
                 || _query.startsWith("get_phone_")) {
             index = Integer.parseInt(_query.substring(_query.lastIndexOf("_") + 1));
+            _query = _query.substring(0, _query.lastIndexOf("_"));
+        }
+
+        if (_query.startsWith("get_id_employee_")) {
+            parameter = _query.substring(_query.lastIndexOf("_") + 1);
             _query = _query.substring(0, _query.lastIndexOf("_"));
         }
 
@@ -100,6 +105,29 @@ public class Server implements Runnable {
                                 "AND p.id_phone_detail = d.id_phone_detail " +
                                 "ORDER BY p.id_phone");
                 break;
+
+            case "get_employees_name":
+                result = db.query(true,
+                        "SELECT CONCAT(last_name, ' ', first_name, ' ', patr_name) " +
+                                "FROM employees " +
+                                "ORDER BY id_employee");
+                break;
+
+            case "get_id_employee":
+                result = db.query(true,
+                        "SELECT id_employee " +
+                                "FROM employees " +
+                                "WHERE CONCAT(last_name, ' ', first_name, ' ', patr_name) = '" + parameter + "'");
+                break;
+
+            case "get_last_sale":
+                result = db.query(true, "SELECT MAX(id_sale) FROM sales");
+                break;
+
+            case "get_last_sale_detail":
+                result = db.query(true, "SELECT MAX(id_sale_detail) FROM sale_details");
+                break;
+
         }
 
         return result;
@@ -133,6 +161,37 @@ public class Server implements Runnable {
                 }
                 break;
 
+            case "add_sale":
+                try {
+                    db.query(false,
+                            "INSERT INTO sales(date_sale, amount,"
+                                    + (values[2].startsWith("id") ? " id_employee," : "") + " is_sold, "
+                                    + "client_name, client_pnumber, client_email) " +
+                                    "VALUES('" + values[0] + "', " + values[1] + ", "
+                                    + (values[2].startsWith("id")
+                                    ? (values[2].substring(2) + ", " + values[3] + ", '"
+                                    + values[4] + "', '" + values[5] + "', '" + values[6] + "'")
+                                    : (values[2] + ", '" + values[3] + "', '" + values[4] + "', '" + values[5] + "'")));
+                } catch (SQLException e) {
+                    result = false;
+                }
+                break;
+
+            case "add_sale_detail":
+                try {
+                    db.query(false, "INSERT INTO sale_details(id_sale) VALUES (" + values[0] + ")");
+                } catch (SQLException e) {
+                    result = false;
+                }
+                break;
+
+            case "add_union_phone":
+                try {
+                    db.query(false, "INSERT INTO union_sale_details_phones " +
+                            "VALUES (" + values[0] + ", " + values[1] + ", " + values[2] + ")");
+                } catch (SQLException e) {
+                    result = false;
+                }
         }
 
         return result;
